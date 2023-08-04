40ee84158d7ea3d04497d17c515d5f2c3dbb8de1
Dhruvil_Android <dhruvil.xitijinfo@gmail.com>
2023-08-04 15:35:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express()
+const port = 9000
+
+
+app.use("/",(req,res)=>{
+    try {
+        res.json({message:"Hello From Express App"})
+        
+    } catch (error) {
+       console.log(error); 
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+})
